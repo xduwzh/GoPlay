647579a0a80ad0c62e4d462fe9f319a98c1238c7
xduwzh <128497005+xduwzh@users.noreply.github.com>
2025-08-15 14:36:41 +0200

    Admin user feature

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,8 +1,16 @@
 // Package models defines the data structures used in the application.
 package models
 
+import "gorm.io/gorm"
+
 type User struct {
 	ID       uint   `gorm:"primaryKey"`
 	Username string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
+	IsAdmin  bool   `gorm:"default:false"` // Indicates if the user is an admin
+	Avatar   string `gorm:"type:varchar(255)"` // URL for the user's avatar
+}
+
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(&User{})
 }
